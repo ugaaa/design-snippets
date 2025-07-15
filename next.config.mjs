cdbff21fd62ac049522aa271d14594ff7fc19ab4
saori ugawa <saori19901002@gmail.com>
2025-07-15 15:49:52 +0900

    デプロイテスト

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 静的エクスポート
+  output: "export", // 静的エクスポート
   trailingSlash: true,
   images: {
-    unoptimized: true,  // 静的エクスポート時は画像最適化を無効
+    unoptimized: true, // 静的エクスポート時は画像最適化を無効
     remotePatterns: [
       {
         protocol: "http",
