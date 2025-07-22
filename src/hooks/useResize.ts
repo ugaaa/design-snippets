5425b370ff91ec5bcfb9a9631ec08d5ca8d76136
saori ugawa <saori19901002@gmail.com>
2025-07-22 10:22:50 +0900

    update size

--- src/hooks/useResize.ts
+++ src/hooks/useResize.ts
@@ -16,7 +16,7 @@ export const useResize = (
 
   const handleResize = useCallback(() => {
     const windowWidth = window.innerWidth;
-    setIsPC(windowWidth >= 780); // Assuming 768px is the breakpoint between PC and SP
+    setIsPC(windowWidth >= 768); // Assuming 768px is the breakpoint between PC and SP
     if (onResize) {
       onResize();
     }
